85687eab6c7896b2e4b4fcb14719a3a077db1a88
Lucas Mac Farland <lucas.farlandmac@gmail.com>
2026-01-11 13:56:04 +0000

    Começo do algoritmo de encriptação, Por enquanto só tentar retornar o valor do input como deveria ir para o outro rotor

--- src/logic/test.ts
+++ src/logic/test.ts
@@ -1,4 +1,16 @@
 export function test(userInput: string) {
-  console.log(userInput)
-  return ("Ola")
+  const alfabeto: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const rotor1: string = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+  let newText: string = "";
+  var n = 0;
+
+  for (var i = 0; i < userInput.length; i++) {
+    while (userInput[i]?.toUpperCase() != alfabeto[n]) {
+      n += 1
+    }
+    var index_entrada = (n + 1) % 26
+    newText += rotor1[(index_entrada - 1 + 26) % 26]
+    n = 0
+  };
+  return newText;
 }
